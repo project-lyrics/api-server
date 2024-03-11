e4b47b03469b3eaf54f8dc7e1f4be5cca55bbd8c
sujeong11 <qalzm7351@gmail.com>
2024-03-11 19:01:19 +0900

    [SCRUM-29] feat: dto 필드에 대한 validation 추가

--- src/main/java/com/projectlyrics/server/domain/artist/dto/request/AddArtistRequest.java
+++ src/main/java/com/projectlyrics/server/domain/artist/dto/request/AddArtistRequest.java
@@ -2,13 +2,17 @@ package com.projectlyrics.server.domain.artist.dto.request;
 
 import com.projectlyrics.server.domain.artist.entity.Artist;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
 
 public record AddArtistRequest(
     @Schema(name = "아티스트의 한글 이름")
+    @Pattern(regexp = "^(?!\\s*$).+", message = "빈 문자열 또는 공백 문자열은 허용하지 않습니다.")
     String name,
     @Schema(name = "아티스트의 영어 이름")
+    @Pattern(regexp = "^[a-zA-Z]+(\\s[a-zA-Z]+)*$", message = "영어 외의 문자열, 빈 문자열 또는 공백 문자열은 허용하지 않습니다.")
     String englishName,
     @Schema(name = "아티스트의 cdn 이미지 경로")
+    @Pattern(regexp = "^https://.*", message = "이미지 경로는 https://로 시작해야 합니다.")
     String profileImageCdnLink
 ) {
 
