dace3f78488eab197c732e4b0a229abb179b456f
sujeong11 <qalzm7351@gmail.com>
2024-03-11 19:03:16 +0900

    [SCRUM-29] test: 인자를 검증하도록 테스트 코드 수정 및 dto 테스트 코드 추가

--- src/test/java/com/projectlyrics/server/domain/artist/service/ArtistServiceTest.java
+++ src/test/java/com/projectlyrics/server/domain/artist/service/ArtistServiceTest.java
@@ -1,22 +1,22 @@
 package com.projectlyrics.server.domain.artist.service;
 
-import com.projectlyrics.server.domain.artist.dto.AddArtistRequest;
+import com.projectlyrics.server.domain.artist.dto.request.AddArtistRequest;
 import com.projectlyrics.server.domain.artist.entity.Artist;
 import com.projectlyrics.server.domain.artist.repository.ArtistRepository;
 import com.projectlyrics.server.domain.artist.service.impl.ArtistServiceImpl;
 import com.projectlyrics.server.utils.ArtistTestUtil;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.doReturn;
-import static org.mockito.Mockito.spy;
 
 @ExtendWith(MockitoExtension.class)
 class ArtistServiceTest {
@@ -27,21 +27,25 @@ class ArtistServiceTest {
   @Mock
   private ArtistRepository artistRepository;
 
+  @Captor
+  private ArgumentCaptor<Artist> addArtistArgumentCaptor;
+
   @Test
   void 전달받은_데이터로_새로운_아티스트를_추가한다() {
     // given
-    Long artistId = 1L;
-    Artist artist = spy(ArtistTestUtil.create());
-    AddArtistRequest addArtistRequest = createAddArtistRequest();
+    var artist = ArtistTestUtil.create();
+    var addArtistRequest = createAddArtistRequest();
     given(artistRepository.save(any(Artist.class))).willReturn(artist);
-    doReturn(artistId).when(artist).getId();
 
     // when
-    Long actualResult = sut.addArtist(addArtistRequest);
+    sut.addArtist(addArtistRequest);
 
     // then
-    then(artistRepository).should().save(any(Artist.class));
-    assertThat(actualResult).isEqualTo(artistId);
+    then(artistRepository).should().save(addArtistArgumentCaptor.capture());
+    var captorValue = addArtistArgumentCaptor.getValue();
+    assertThat(addArtistRequest.name()).isEqualTo(captorValue.getName());
+    assertThat(addArtistRequest.englishName()).isEqualTo(captorValue.getEnglishName());
+    assertThat(addArtistRequest.profileImageCdnLink()).isEqualTo(captorValue.getProfileImageCdnLink());
   }
 
   private AddArtistRequest createAddArtistRequest() {
