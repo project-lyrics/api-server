fd2a61bf6d55abbca16fd969ef44769ae2fd04e1
sujeong11 <qalzm7351@gmail.com>
2024-03-12 00:21:24 +0900

    [SCRUM-29] fix: 잘못된 정규식 수정

--- src/test/java/com/projectlyrics/server/domain/artist/dto/request/AddArtistRequestTest.java
+++ src/test/java/com/projectlyrics/server/domain/artist/dto/request/AddArtistRequestTest.java
@@ -28,8 +28,8 @@ class AddArtistRequestTest {
   }
 
   @ParameterizedTest
-  @ValueSource(strings = {"", "     ", "넬"})
-  void 아티스트의_영어_이름이_영어_이외_문자열이나_빈_문자열_또는_공백_문자열이라면_validation_에러가_발생한다(String englishName) {
+  @ValueSource(strings = {"", "     "})
+  void 아티스트의_영어_이름이_문자열이나_빈_문자열_또는_공백_문자열이라면_validation_에러가_발생한다(String englishName) {
     // given
     var addArtistRequest = createAddArtistRequest("넬", englishName, "https://~");
 
@@ -38,7 +38,7 @@ class AddArtistRequestTest {
 
     // then
     violation.forEach(error -> {
-      assertThat(error.getMessage()).isEqualTo("영어 외의 문자열, 빈 문자열 또는 공백 문자열은 허용하지 않습니다.");
+      assertThat(error.getMessage()).isEqualTo("빈 문자열 또는 공백 문자열은 허용하지 않습니다.");
     });
   }
 
