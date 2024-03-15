9a3c993f7b8b1f4cbf8aad272e7f23e6df78a1df
sujeong11 <qalzm7351@gmail.com>
2024-03-15 12:04:31 +0900

    [SCRUM-29] test: 아티스트 리스트 조회 API의 테스트 코드 작성

--- src/test/java/com/projectlyrics/server/domain/artist/service/ArtistQueryServiceTest.java
+++ src/test/java/com/projectlyrics/server/domain/artist/service/ArtistQueryServiceTest.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.doReturn;
 import com.projectlyrics.server.domain.artist.repository.CommandQueryArtistRepository;
 import com.projectlyrics.server.domain.artist.service.impl.ArtistQueryServiceImpl;
 import com.projectlyrics.server.utils.ArtistTestUtil;
+import java.util.List;
 import java.util.Optional;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -16,6 +17,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.SliceImpl;
 
 @ExtendWith(MockitoExtension.class)
 class ArtistQueryServiceTest {
@@ -43,5 +46,29 @@ class ArtistQueryServiceTest {
     assertThat(getArtistResponse.name()).isEqualTo(artist.getName());
     assertThat(getArtistResponse.englishName()).isEqualTo(artist.getEnglishName());
     assertThat(getArtistResponse.profileImageCdnLink()).isEqualTo(artist.getProfileImageCdnLink());
+  }
+
+  @Test
+  void 아티스트의_데이터를_커서_기반_페이지네이션으로_조회해_반환한다() {
+    // given
+    var pageable = Pageable.ofSize(15);
+    var artistList = List.of(
+        ArtistTestUtil.createWithName("실리카겔"),
+        ArtistTestUtil.createWithName("잔나비")
+    );
+    given(commandArtistRepository.findAllAndNotDeleted(pageable)).willReturn(new SliceImpl<>(artistList, pageable, false));
+
+    // when
+    var artistListResponse = sut.getArtistList(pageable);
+
+    // then
+    then(commandArtistRepository).should().findAllAndNotDeleted(pageable);
+    assertThat(artistListResponse.currentCursor()).isEqualTo("0");
+    assertThat(artistListResponse.nextCursor()).isEqualTo(null);
+    assertThat(artistListResponse.itemSize()).isEqualTo(artistList.size());
+    assertThat(artistListResponse.totalSize()).isEqualTo(pageable.getPageSize());
+    for (int i = 0; i < artistList.size(); i++) {
+      assertThat(artistListResponse.data().get(i).name()).isEqualTo(artistList.get(i).getName());
+    }
   }
 }
