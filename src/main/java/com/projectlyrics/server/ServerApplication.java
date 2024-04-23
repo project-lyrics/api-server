38c40d710050512d77cc5e0216ca169289981923
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-23 16:01:32 +0900

    [SCRUM-60] refactor: feign client 위한 configuration 파일 작성

--- src/main/java/com/projectlyrics/server/ServerApplication.java
+++ src/main/java/com/projectlyrics/server/ServerApplication.java
@@ -2,9 +2,7 @@ package com.projectlyrics.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableFeignClients
 @SpringBootApplication
 public class ServerApplication {
 
