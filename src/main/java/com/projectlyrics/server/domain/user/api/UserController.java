14974e2fdf23120de76b16408287e579ed85d2d1
Jin Geonwoo <jinkonu99@gmail.com>
2024-04-25 18:07:09 +0900

    [SCRUM-60] feat: 사용자 컨트롤러에 Swagger 전용 인터페이스 적용

--- src/main/java/com/projectlyrics/server/domain/user/api/UserController.java
+++ src/main/java/com/projectlyrics/server/domain/user/api/UserController.java
@@ -1,10 +1,8 @@
 package com.projectlyrics.server.domain.user.api;
 
+import com.projectlyrics.server.domain.auth.external.dto.request.UserLoginRequest;
 import com.projectlyrics.server.domain.user.dto.response.LoginResponse;
-import com.projectlyrics.server.domain.user.service.UserCommandService;
-import com.projectlyrics.server.global.auth.external.dto.request.UserLoginRequest;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.projectlyrics.server.domain.user.usecase.command.UserAuthUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,24 +11,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "사용자 관련 API")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 @RestController
-public class UserController {
+public class UserController implements UserControllerSwagger {
 
-  private final UserCommandService userCommandService;
+  private final UserAuthUseCase userAuthUseCase;
 
-  @Operation(
-      summary = "사용자 로그인 API",
-      description = "인가 코드와 로그인 요청 데이터를 받아 사용자를 가입시킵니다."
-  )
   @PostMapping("/login/oauth2/kakao")
   public ResponseEntity<LoginResponse> login(
       @RequestBody UserLoginRequest loginRequest
   ) {
     return ResponseEntity
         .status(HttpStatus.CREATED)
-        .body(userCommandService.login(loginRequest));
+        .body(userAuthUseCase.login(loginRequest));
   }
 }
---
+++ src/main/java/com/projectlyrics/server/domain/user/api/UserControllerSwagger.java
@@ -0,0 +1,35 @@
+package com.projectlyrics.server.domain.user.api;
+
+import com.projectlyrics.server.domain.auth.external.dto.request.UserLoginRequest;
+import com.projectlyrics.server.domain.common.dto.ErrorResponse;
+import com.projectlyrics.server.domain.user.dto.response.LoginResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Tag(name = "사용자 관련 API")
+public interface UserControllerSwagger {
+
+    @Operation(
+            summary = "사용자 로그인 API",
+            description = "인가 코드와 로그인 요청 데이터를 받아 사용자를 가입시킵니다."
+    )
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "소셜 로그인이 완료되었습니다."),
+                    @ApiResponse(responseCode = "400", description =
+                            "1. 요청한 값이 유효하지 않습니다.\n" + "2. 인가 코드가 만료되었습니다.\n",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+                    @ApiResponse(responseCode = "500", description = "서버 내부 오류입니다.",
+                            content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
+            }
+    )
+    ResponseEntity<LoginResponse> login(
+            @RequestBody UserLoginRequest loginRequest
+    );
+}
