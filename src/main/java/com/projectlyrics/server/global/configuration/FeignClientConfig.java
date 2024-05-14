dfc578f17929d42bd6d47a2eedbc0b9b90f20af7
Jin Geonwoo <jinkonu99@gmail.com>
2024-05-14 15:26:40 +0900

    [SCRUM-70] remove: 불필요한 카카오 로그인 로직 제거

--- src/main/java/com/projectlyrics/server/global/configuration/FeignClientConfig.java
+++ src/main/java/com/projectlyrics/server/global/configuration/FeignClientConfig.java
@@ -1,13 +1,11 @@
 package com.projectlyrics.server.global.configuration;
 
 import com.projectlyrics.server.domain.auth.service.kakao.KakaoSocialDataApiClient;
-import com.projectlyrics.server.domain.test.auth.kakao.KakaoAuthApiClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(basePackageClasses = {
-    KakaoAuthApiClient.class,
     KakaoSocialDataApiClient.class
 })
 public class FeignClientConfig {
