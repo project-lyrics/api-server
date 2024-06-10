e9f3612c0661710d5d59b2868826cb2af890b391
Jin Geonwoo <jinkonu99@gmail.com>
2024-06-10 18:27:43 +0900

    [SCRUM-109] refactor: ArtistController 내 메서드에 공통 응답 추가

--- src/main/java/com/projectlyrics/server/domain/artist/entity/Artist.java
+++ src/main/java/com/projectlyrics/server/domain/artist/entity/Artist.java
@@ -1,8 +1,8 @@
 package com.projectlyrics.server.domain.artist.entity;
 
 import com.projectlyrics.server.domain.common.entity.EntityCommonField;
-import com.projectlyrics.server.global.exception.FeelinException;
 import com.projectlyrics.server.domain.common.message.ErrorCode;
+import com.projectlyrics.server.global.exception.FeelinException;
 import jakarta.persistence.Column;
 import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
