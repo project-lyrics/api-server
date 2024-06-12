a401580ef52b01cfadb6d55350779302e6002434
wlsh44 <k87913j@naver.com>
2024-06-12 17:31:54 +0900

    [SCRUM-110] test: artistController 테스트 코드 일부 추가

--- src/test/java/com/projectlyrics/server/common/ControllerTest.java
+++ src/test/java/com/projectlyrics/server/common/ControllerTest.java
@@ -3,16 +3,22 @@ package com.projectlyrics.server.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.projectlyrics.server.domain.artist.service.ArtistCommandService;
 import com.projectlyrics.server.domain.artist.service.ArtistQueryService;
+import com.projectlyrics.server.domain.auth.jwt.JwtTokenProvider;
+import com.projectlyrics.server.domain.auth.jwt.dto.AuthToken;
 import com.projectlyrics.server.domain.user.service.UserCommandService;
 import com.projectlyrics.server.domain.record.service.RecordCommandService;
 import com.projectlyrics.server.domain.record.service.RecordQueryService;
 import com.projectlyrics.server.domain.user.service.UserQueryService;
+import com.projectlyrics.server.global.handler.FilterExceptionHandler;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.servlet.MockMvc;
 
 @WebMvcTest
+@Import({JwtTokenProvider.class, FilterExceptionHandler.class})
 public abstract class ControllerTest {
 
     @Autowired
@@ -20,6 +26,9 @@ public abstract class ControllerTest {
 
     @Autowired
     protected ObjectMapper mapper;
+
+    @Autowired
+    private JwtTokenProvider jwtTokenProvider;
 
     @MockBean
     protected ArtistCommandService artistCommandService;
@@ -39,4 +48,11 @@ public abstract class ControllerTest {
     @MockBean
     protected UserCommandService userCommandService;
 
+    protected String accessToken;
+
+    @BeforeEach
+    public void setUp() {
+        AuthToken authToken = jwtTokenProvider.issueTokens(1L);
+        accessToken = authToken.accessToken();
+    }
 }
--- src/test/java/com/projectlyrics/server/domain/artist/api/ArtistControllerTest.java
+++ src/test/java/com/projectlyrics/server/domain/artist/api/ArtistControllerTest.java
@@ -7,34 +7,45 @@ import com.projectlyrics.server.domain.artist.dto.response.ArtistAddResponse;
 import com.projectlyrics.server.domain.common.dto.SuccessResponse;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class ArtistControllerTest extends ControllerTest {
 
   @Test
+  @WithMockUser
   void 아티스트를_추가해야_한다() throws Exception {
     //given
-    ArtistAddRequest request = new ArtistAddRequest("라디오헤드", "radiohead", "http://~");
+    ArtistAddRequest request = new ArtistAddRequest("라디오헤드", "radiohead", "https://~");
 
     //when then
     mockMvc.perform(post("/api/v1/artists")
+            .with(csrf())
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
             .contentType(MediaType.APPLICATION_JSON)
             .content(mapper.writeValueAsString(request)))
-        .andExpect(status().isOk());
+        .andDo(print())
+        .andExpect(status().isCreated());
   }
 
   @Test
+  @WithMockUser
   void 아티스트를_수정해야_한다() throws Exception {
     //given
-    ArtistUpdateRequest request = new ArtistUpdateRequest("라디오헤드", "radiohead", "http://~");
+    ArtistUpdateRequest request = new ArtistUpdateRequest("라디오헤드", "radiohead", "https://~");
 
     //when then
     mockMvc.perform(patch("/api/v1/artists/{artistId}", 1)
+            .with(csrf())
+            .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
             .contentType(MediaType.APPLICATION_JSON)
             .content(mapper.writeValueAsString(request)))
         .andExpect(status().isOk());
