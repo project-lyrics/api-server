5f781ebc7eb247a1601eef3a678b59f475fc9b69
wlsh44 <k87913j@naver.com>
2024-06-21 16:44:32 +0900

    [SCRUM-120] test: 로그인 테스트 코드 추가

--- src/test/java/com/projectlyrics/server/domain/auth/service/AuthCommandServiceIntegrationTest.java
+++ src/test/java/com/projectlyrics/server/domain/auth/service/AuthCommandServiceIntegrationTest.java
@@ -1,25 +1,37 @@
 package com.projectlyrics.server.domain.auth.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.doReturn;
 
 import com.projectlyrics.server.common.IntegrationTest;
 import com.projectlyrics.server.common.fixture.UserFixture;
 import com.projectlyrics.server.domain.auth.dto.request.AuthSignInRequest;
-import com.projectlyrics.server.domain.auth.dto.response.AuthLoginResponse;
+import com.projectlyrics.server.domain.auth.dto.response.AuthTokenResponse;
 import com.projectlyrics.server.domain.auth.entity.enumerate.AuthProvider;
 import com.projectlyrics.server.domain.auth.jwt.JwtTokenProvider;
+import com.projectlyrics.server.domain.auth.service.dto.AuthSocialInfo;
+import com.projectlyrics.server.domain.auth.service.social.apple.ApplePublicKeysApiClient;
+import com.projectlyrics.server.domain.auth.service.social.apple.AppleSocialService;
+import com.projectlyrics.server.domain.auth.service.social.apple.dto.AppleUserInfoResponse;
 import com.projectlyrics.server.domain.auth.service.social.kakao.KakaoSocialDataApiClient;
 import com.projectlyrics.server.domain.auth.service.social.kakao.dto.KakaoAccount;
 import com.projectlyrics.server.domain.auth.service.social.kakao.dto.KakaoUserInfoResponse;
 import com.projectlyrics.server.domain.user.entity.User;
+import com.projectlyrics.server.domain.user.exception.UserNotFoundException;
 import com.projectlyrics.server.domain.user.repository.UserCommandRepository;
 import com.projectlyrics.server.domain.user.repository.UserQueryRepository;
+import feign.FeignException;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.Optional;
 
 public class AuthCommandServiceIntegrationTest extends IntegrationTest {
 
@@ -33,39 +45,64 @@ public class AuthCommandServiceIntegrationTest extends IntegrationTest {
     UserQueryRepository userQueryRepository;
 
     @Autowired
+    AuthQueryService authQueryService;
+
+    @Autowired
     JwtTokenProvider jwtTokenProvider;
 
-    @MockBean
+    @SpyBean
     KakaoSocialDataApiClient kakaoSocialDataApiClient;
+
+    @SpyBean
+    AppleSocialService appleSocialService;
 
     @Test
     void 카카오_계정으로_로그인_해야_한다() throws Exception {
         //given
-        String accessToken = "accessToken";
+        String socialAccessToken = "accessToken";
         User savedUser = userCommandRepository.save(UserFixture.createKakao());
-        given(kakaoSocialDataApiClient.getUserInfo(any()))
-                .willReturn(new KakaoUserInfoResponse(savedUser.getAuth().getSocialId(), new KakaoAccount(savedUser.getEmail())));
+        doReturn(new KakaoUserInfoResponse(savedUser.getAuth().getSocialId(), new KakaoAccount(savedUser.getEmail())))
+                .when(kakaoSocialDataApiClient).getUserInfo(any());
 
         //when
-        AuthLoginResponse response = sut.signIn(new AuthSignInRequest(savedUser.getAuth().getSocialId(), AuthProvider.KAKAO));
+        AuthTokenResponse response = sut.signIn(new AuthSignInRequest(socialAccessToken, AuthProvider.KAKAO));
 
         //then
         Long userId = jwtTokenProvider.getUserIdFromJwt(response.accessToken());
         assertThat(userId).isEqualTo(savedUser.getId());
     }
 
-    //TODO apple 로그인 기능 추가 후 테스트
     @Test
-    @Disabled("apple 로그인 기능 추가 후 테스트")
+    void 없는_유저인_경우_로그인에_실패해야_한다() throws Exception {
+        //given
+        String socialAccessToken = "accessToken";
+        doReturn(new KakaoUserInfoResponse("socialId", new KakaoAccount("email")))
+                .when(kakaoSocialDataApiClient).getUserInfo(any());
+
+        //when then
+        assertThatThrownBy(() -> sut.signIn(new AuthSignInRequest(socialAccessToken, AuthProvider.KAKAO)))
+                .isInstanceOf(UserNotFoundException.class);
+    }
+
+    @Test
+    void 소셜_인증에_실패한_경우_로그인에_실패해야_한다() throws Exception {
+        //given
+        String socialAccessToken = "accessToken";
+
+        //when then
+        assertThatThrownBy(() -> sut.signIn(new AuthSignInRequest(socialAccessToken, AuthProvider.KAKAO)))
+                .isInstanceOf(FeignException.class);
+    }
+
+    @Test
     void 애플_계정으로_로그인_해야_한다() throws Exception {
         //given
         String accessToken = "accessToken";
         User savedUser = userCommandRepository.save(UserFixture.createApple());
-        given(kakaoSocialDataApiClient.getUserInfo(any()))
-                .willReturn(new KakaoUserInfoResponse(savedUser.getAuth().getSocialId(), new KakaoAccount(savedUser.getEmail())));
-
+        doReturn(new AuthSocialInfo(AuthProvider.APPLE, savedUser.getAuth().getSocialId(), savedUser.getEmail()))
+                .when(appleSocialService).getSocialData(any());
         //when
-        AuthLoginResponse response = sut.signIn(new AuthSignInRequest(accessToken, AuthProvider.APPLE));
+        AuthTokenResponse response = sut.signIn(new AuthSignInRequest(accessToken, AuthProvider.APPLE));
 
         //then
         Long userId = jwtTokenProvider.getUserIdFromJwt(response.accessToken());
