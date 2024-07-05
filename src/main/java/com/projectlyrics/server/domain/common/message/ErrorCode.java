bca9a1234f79a099c90dfc904a9c212a932636e8
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-05 15:42:23 +0900

    [SCRUM-134] chore: ErrorCode 내 에러 메시지 일부 한글화

--- src/main/java/com/projectlyrics/server/domain/common/message/ErrorCode.java
+++ src/main/java/com/projectlyrics/server/domain/common/message/ErrorCode.java
@@ -10,8 +10,8 @@ public enum ErrorCode {
 
     // Common
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "00000", "Invalid request."),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "00001", "Some request field is missing or invalid."),
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "00002", "An unexpected error occurred on the server."),
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "00001", "일부 필드가 잘못된 값으로 입력되었습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "00002", "서버 내부에 오류가 발생했습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "00003", "Not Found"),
     NULL_FIELD(HttpStatus.BAD_REQUEST, "00004", "Some field is missing."),
     EMPTY_FIELD(HttpStatus.BAD_REQUEST, "00005", "Some field is empty."),
@@ -23,9 +23,9 @@ public enum ErrorCode {
     // Auth
     REFRESH_TOKEN_NOT_FOUND(HttpStatus.NOT_FOUND, "01000", "The refresh token could not be found."),
     TOKEN_EXPIRED(HttpStatus.BAD_REQUEST, "01001", "The token is expired."),
-    WRONG_TOKEN_TYPE(HttpStatus.BAD_REQUEST, "01002", "Wrong token type is passed."),
+    WRONG_TOKEN_TYPE(HttpStatus.BAD_REQUEST, "01002", "잘못된 토큰 형식으로 입력되었습니다."),
     UNSUPPORTED_AUTH_PROVIDER(HttpStatus.BAD_REQUEST, "01003", "It is unsupported authentication provider"),
-    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "01004", "The token is not valid."),
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "01004", "토큰이 유효하지 않습니다."),
     INVALID_KEY(HttpStatus.UNAUTHORIZED, "01005", "The key is not valid."),
     INVALID_AUTH_SECRET_KEY(HttpStatus.UNAUTHORIZED, "01006", "The secret key is not valid."),
     NOT_AGREE_TO_TERMS(HttpStatus.BAD_REQUEST, "01007", "Not agree to terms and conditions"),
