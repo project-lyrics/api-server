e4fbbeef8f58e4493ec83ddfa050db00a60ec18b
wlsh44 <k87913j@naver.com>
2024-07-16 21:48:29 +0900

    [SCRUM-142] test: 아티스트 리스트 조회 테스트 코드 추가

--- src/test/java/com/projectlyrics/server/support/RestDocsTest.java
+++ src/test/java/com/projectlyrics/server/support/RestDocsTest.java
@@ -1,10 +1,14 @@
 package com.projectlyrics.server.support;
 
+import com.epages.restdocs.apispec.HeaderDescriptorWithType;
+import com.epages.restdocs.apispec.ParameterDescriptorWithType;
+import com.epages.restdocs.apispec.SimpleType;
 import com.projectlyrics.server.support.config.RestDocsConfiguration;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpHeaders;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
@@ -18,6 +22,8 @@ import org.springframework.web.context.WebApplicationContext;
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.parameterWithName;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.restdocs.snippet.Attributes.Attribute;
 
@@ -34,12 +40,28 @@ public abstract class RestDocsTest extends ControllerTest {
         return new Attribute("constraint", value);
     }
 
+    protected static HeaderDescriptorWithType getAuthorizationHeader() {
+        return headerWithName(HttpHeaders.AUTHORIZATION).type(SimpleType.STRING)
+                .description("Bearer ${accessToken}");
+    }
+
     protected static FieldDescriptor[] getErrorResponseFields() {
         return new FieldDescriptor[]{
                 fieldWithPath("errorCode").type(JsonFieldType.STRING)
                         .description("에러코드"),
                 fieldWithPath("errorMessage").type(JsonFieldType.STRING)
                         .description("메세지")
+        };
+    }
+
+    protected static ParameterDescriptorWithType[] getPagingQueryParameters() {
+        return new ParameterDescriptorWithType[]{
+                parameterWithName("cursor").type(SimpleType.STRING)
+                        .optional()
+                        .description("이전에 응답 받은 nextCursor 값, 해당 cursor보다 큰 id를 가진 아티스트 조회"),
+                parameterWithName("size").type(SimpleType.NUMBER)
+                        .optional()
+                        .description("데이터 수 (default = 10)")
         };
     }
 
