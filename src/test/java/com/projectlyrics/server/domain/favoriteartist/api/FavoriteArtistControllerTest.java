0b33e038087770e5d5969b13a8b4002c41b80fcb
wlsh44 <k87913j@naver.com>
2024-07-16 17:30:02 +0900

    [SCRUM-141] test: 관심 아티스트 api 테스트 코드 수정

--- src/test/java/com/projectlyrics/server/domain/favoriteartist/api/FavoriteArtistControllerTest.java
+++ src/test/java/com/projectlyrics/server/domain/favoriteartist/api/FavoriteArtistControllerTest.java
@@ -1,17 +1,25 @@
 package com.projectlyrics.server.domain.favoriteartist.api;
 
-import com.projectlyrics.server.support.ControllerTest;
+import com.epages.restdocs.apispec.ResourceSnippetParameters;
+import com.epages.restdocs.apispec.Schema;
+import com.epages.restdocs.apispec.SimpleType;
 import com.projectlyrics.server.domain.favoriteartist.dto.request.CreateFavoriteArtistListRequest;
+import com.projectlyrics.server.support.RestDocsTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.mockmvc.RestDocumentationResultHandler;
+import org.springframework.restdocs.payload.JsonFieldType;
 
 import java.util.List;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.epages.restdocs.apispec.ResourceDocumentation.headerWithName;
+import static com.epages.restdocs.apispec.ResourceDocumentation.resource;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-class FavoriteArtistControllerTest extends ControllerTest {
+class FavoriteArtistControllerTest extends RestDocsTest {
 
     @Test
     void 관심_아티스트들을_저장해야_한다() throws Exception {
@@ -23,6 +31,25 @@ class FavoriteArtistControllerTest extends ControllerTest {
                         .header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(mapper.writeValueAsString(request)))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andDo(getCreateFavoriteArtistListDocument());
+    }
+
+    private RestDocumentationResultHandler getCreateFavoriteArtistListDocument() {
+        return restDocs.document(
+                resource(ResourceSnippetParameters.builder()
+                        .tag("Favorite Artist API")
+                        .summary("관심 아티스트 리스트 추가 API")
+                        .requestHeaders(
+                                headerWithName(HttpHeaders.AUTHORIZATION).type(SimpleType.STRING)
+                                        .description("Bearer ${accessToken}")
+                        )
+                        .requestFields(
+                                fieldWithPath("artistIds").type(JsonFieldType.ARRAY)
+                                        .description("Artist Id 배열(number) ex) [1, 2, 3]")
+                        )
+                        .requestSchema(Schema.schema("Create Favorite Artist List Request"))
+                        .build())
+        );
     }
 }
