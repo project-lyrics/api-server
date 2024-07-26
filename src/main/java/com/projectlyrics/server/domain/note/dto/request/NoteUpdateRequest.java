529a44ac56afdede75020a255ab65bbf298354b3
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-26 15:22:11 +0900

    [SCRUM-147] chore: request DTO 내 스웨거 코드 제거

--- src/main/java/com/projectlyrics/server/domain/note/dto/request/NoteUpdateRequest.java
+++ src/main/java/com/projectlyrics/server/domain/note/dto/request/NoteUpdateRequest.java
@@ -2,22 +2,15 @@ package com.projectlyrics.server.domain.note.dto.request;
 
 import com.projectlyrics.server.domain.note.entity.NoteBackground;
 import com.projectlyrics.server.domain.note.entity.NoteStatus;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 
 public record NoteUpdateRequest(
         @NotNull(message = "노트 ID가 입력되지 않았습니다.")
-        @Schema(name = "노트 ID")
         Long noteId,
-        @Schema(name = "노트 내용")
         String content,
-        @Schema(name = "가사")
         String lyrics,
-        @Schema(name = "배경색")
         NoteBackground background,
-        @Schema(name = "노트 상태")
         NoteStatus status,
-        @Schema(name = "작성자 ID")
         Long publisherId
 ) {
 }
--- src/main/java/com/projectlyrics/server/domain/song/dto/request/SongCreateRequest.java
+++ src/main/java/com/projectlyrics/server/domain/song/dto/request/SongCreateRequest.java
@@ -1,7 +1,6 @@
 package com.projectlyrics.server.domain.song.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -10,23 +9,17 @@ import java.time.LocalDate;
 
 public record SongCreateRequest(
         @NotBlank(message = "스포티파이 ID가 입력되지 않았습니다.")
-        @Schema(name = "스포티파이 ID")
         String spotifyId,
         @NotBlank(message = "곡 제목이 입력되지 않았습니다.")
-        @Schema(name = "곡 제목")
         String name,
         @NotBlank(message = "곡 발매일이 입력되지 않았습니다.")
         @DateTimeFormat(pattern = "yyyy-MM-dd")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
-        @Schema(name = "곡 발매일")
         LocalDate releaseDate,
         @NotBlank(message = "앨범 이름이 입력되지 않았습니다.")
-        @Schema(name = "앨범 이름")
         String albumName,
-        @Schema(name = "곡 이미지 URL")
         String imageUrl,
         @NotNull
-        @Schema(name = "아티스트 ID")
         Long artistId
 ) {
 }
