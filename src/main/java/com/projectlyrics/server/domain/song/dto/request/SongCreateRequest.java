18d023fdd316573d4db9f923b9526299fa7c5510
Jin Geonwoo <jinkonu99@gmail.com>
2024-07-31 16:44:15 +0900

    [SCRUM-147] fix: SongCreateRequest 내 날짜 포맷팅 중복 제거

--- src/main/java/com/projectlyrics/server/domain/song/dto/request/SongCreateRequest.java
+++ src/main/java/com/projectlyrics/server/domain/song/dto/request/SongCreateRequest.java
@@ -3,7 +3,6 @@ package com.projectlyrics.server.domain.song.dto.request;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -13,7 +12,6 @@ public record SongCreateRequest(
         @NotBlank(message = "곡 제목이 입력되지 않았습니다.")
         String name,
         @NotBlank(message = "곡 발매일이 입력되지 않았습니다.")
-        @DateTimeFormat(pattern = "yyyy-MM-dd")
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
         LocalDate releaseDate,
         @NotBlank(message = "앨범 이름이 입력되지 않았습니다.")
