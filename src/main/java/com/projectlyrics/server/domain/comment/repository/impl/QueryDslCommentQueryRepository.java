dcb6c151a0d8e6ac70e3ea6cf44f4a40c4f46d86
Jin Geonwoo <jinkonu99@gmail.com>
2024-08-08 17:08:26 +0900

    [SCRUM-155] feat: 노트와 관련된 댓글 개수 조회와 댓글 객체 리스트 조회 리포지토리 메서드 추가

--- src/main/java/com/projectlyrics/server/domain/comment/repository/impl/QueryDslCommentQueryRepository.java
+++ src/main/java/com/projectlyrics/server/domain/comment/repository/impl/QueryDslCommentQueryRepository.java
@@ -2,10 +2,15 @@ package com.projectlyrics.server.domain.comment.repository.impl;
 
 import com.projectlyrics.server.domain.comment.domain.Comment;
 import com.projectlyrics.server.domain.comment.repository.CommentQueryRepository;
+import com.projectlyrics.server.domain.common.util.QueryDslUtils;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.SliceImpl;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import static com.projectlyrics.server.domain.comment.domain.QComment.comment;
@@ -29,5 +34,35 @@ public class QueryDslCommentQueryRepository implements CommentQueryRepository {
                         )
                         .fetchOne()
         );
+    }
+
+    @Override
+    public Slice<Comment> findAllByNoteId(Long noteId, Long cursorId, Pageable pageable) {
+        List<Comment> content = jpaQueryFactory
+                .selectFrom(comment)
+                .leftJoin(comment.writer).fetchJoin()
+                .leftJoin(comment.note).fetchJoin()
+                .where(
+                        comment.note.id.eq(noteId),
+                        comment.deletedAt.isNull(),
+                        QueryDslUtils.gtCursorId(cursorId, comment.id)
+                )
+                .orderBy(comment.id.desc())
+                .limit(pageable.getPageSize() + 1)
+                .fetch();
+
+        return new SliceImpl<>(content, pageable, QueryDslUtils.checkIfHasNext(pageable, content));
+    }
+
+    @Override
+    public long countByNoteId(Long noteId) {
+        return jpaQueryFactory
+                .select(comment.count())
+                .from(comment)
+                .where(
+                        comment.note.id.eq(noteId),
+                        comment.deletedAt.isNull()
+                )
+                .fetchOne();
     }
 }
