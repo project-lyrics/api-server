9a9bf03f2a8a7c9b43ec3c0082df98dce1094806
Jin Geonwoo <jinkonu99@gmail.com>
2024-08-20 03:26:01 +0900

    [SCRUM-161] refactor: 비로그인 유저용 API 허용하도록 수정

--- src/main/java/com/projectlyrics/server/global/configuration/WebConfig.java
+++ src/main/java/com/projectlyrics/server/global/configuration/WebConfig.java
@@ -25,7 +25,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .addPathPatterns("/api/**")
                 .excludePathPatterns("/api/v1/auth/sign-in")
                 .excludePathPatterns("/api/v1/auth/sign-up")
-                .excludePathPatterns("/api/v1/auth/token");
+                .excludePathPatterns("/api/v1/auth/token")
+                .excludePathPatterns("/api/v1/artists/search")
+                .excludePathPatterns("/api/v1/notes/artists")
+                .excludePathPatterns("/api/v1/notes/songs")
+                .excludePathPatterns("/api/v1/songs/search");
     }
 
     @Override
