d63c7bd493283bec99504ecade3812bd519e53a3
Jin Geonwoo <jinkonu99@gmail.com>
2024-08-25 12:55:18 +0900

    [SCRUM-164] test: 댓글 측 이벤트 발행 여부 테스트 추가

---
+++ src/test/java/com/projectlyrics/server/domain/notification/service/NotificationCommandServiceTest.java
@@ -0,0 +1,61 @@
+package com.projectlyrics.server.domain.notification.service;
+
+import com.projectlyrics.server.domain.comment.domain.Comment;
+import com.projectlyrics.server.domain.comment.dto.request.CommentCreateRequest;
+import com.projectlyrics.server.domain.comment.repository.CommentCommandRepository;
+import com.projectlyrics.server.domain.comment.repository.CommentQueryRepository;
+import com.projectlyrics.server.domain.comment.service.CommentCommandService;
+import com.projectlyrics.server.domain.note.entity.Note;
+import com.projectlyrics.server.domain.note.repository.NoteQueryRepository;
+import com.projectlyrics.server.domain.notification.domain.event.CommentEvent;
+import com.projectlyrics.server.domain.user.entity.User;
+import com.projectlyrics.server.domain.user.repository.UserQueryRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class NotificationCommandServiceTest {
+
+    @Mock
+    private CommentCommandRepository commentCommandRepository;
+
+    @Mock
+    private CommentQueryRepository commentQueryRepository;
+
+    @Mock
+    private UserQueryRepository userQueryRepository;
+
+    @Mock
+    private NoteQueryRepository noteQueryRepository;
+
+    @Mock
+    private ApplicationEventPublisher eventPublisher;
+
+    @InjectMocks
+    private CommentCommandService commentCommandService;
+
+    @Test
+    void 댓글을_저장할_때_이벤트가_발행된다() {
+        // given
+        CommentCreateRequest request = new CommentCreateRequest("댓글 내용", 1L);
+        when(userQueryRepository.findById(anyLong())).thenReturn(Optional.of(mock(User.class)));
+        when(noteQueryRepository.findById(anyLong())).thenReturn(Optional.of(mock(Note.class)));
+        when(commentCommandRepository.save(any(Comment.class))).thenReturn(mock(Comment.class));
+
+        // when
+        commentCommandService.create(request, 1L);
+
+        // then
+        verify(eventPublisher).publishEvent(any(CommentEvent.class));
+    }
+}
