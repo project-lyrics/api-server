9dbd9c578c4557373560eff77728ecac9409dd20
Jin Geonwoo <jinkonu99@gmail.com>
2024-08-25 14:02:27 +0900

    [SCRUM-164] feat: 수신자별 알림 리스트 조회 추가

--- src/main/java/com/projectlyrics/server/domain/notification/repository/NotificationQueryRepository.java
+++ src/main/java/com/projectlyrics/server/domain/notification/repository/NotificationQueryRepository.java
@@ -1,4 +1,10 @@
 package com.projectlyrics.server.domain.notification.repository;
 
+import com.projectlyrics.server.domain.notification.domain.Notification;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+
 public interface NotificationQueryRepository {
+
+    Slice<Notification> findAllByReceiverId(Long receiverId, Long cursorId, Pageable pageable);
 }
