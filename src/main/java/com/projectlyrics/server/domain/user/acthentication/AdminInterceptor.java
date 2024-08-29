3798c438e73b83a3fef9f59be128616fbf047597
sebbbin <nsb08045@daum.net>
2024-08-29 10:25:58 +0900

    [SCRUM-166] feat:AdminInterceptor

--- src/main/java/com/projectlyrics/server/domain/user/acthentication/AdminInterceptor.java
+++ src/main/java/com/projectlyrics/server/domain/user/acthentication/AdminInterceptor.java
@@ -1,36 +1,35 @@
-package com.projectlyrics.server.domain.auth.authentication;
+package com.projectlyrics.server.domain.user.acthentication;
 
+import com.projectlyrics.server.domain.auth.authentication.AuthContext;
+import com.projectlyrics.server.domain.auth.authentication.TokenExtractor;
 import com.projectlyrics.server.domain.auth.authentication.jwt.JwtClaim;
 import com.projectlyrics.server.domain.auth.authentication.jwt.JwtExtractor;
+import com.projectlyrics.server.domain.user.entity.Role;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 @RequiredArgsConstructor
-public class AuthInterceptor implements HandlerInterceptor {
+public class AdminInterceptor implements HandlerInterceptor {
 
     private final AuthContext authContext;
-    private final TokenExtractor tokenExtractor;
     private final JwtExtractor jwtExtractor;
+    private final TokenExtractor tokenExtractor;
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (isExcluded(request)) {
+        setAuthContext(request);
+
+        if (Role.ADMIN.equals(authContext.getRole())) {
             return true;
         }
 
-        setAuthContext(request);
-        return true;
-    }
-
-    private boolean isExcluded(HttpServletRequest request) {
-        return request.getRequestURI().matches("/api/v1/notes/\\d+") &&
-                request.getMethod().equalsIgnoreCase(HttpMethod.GET.name());
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        return false;
     }
 
     private void setAuthContext(HttpServletRequest request) {
