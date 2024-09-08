47ff8b8d4f9a56b757271e0ac131ad721ba6622d
elive7 <parksub020704@gmail.com>
2024-09-08 20:46:10 +0900

    [SCRUM-172] feat: slack block UI 수정

--- src/main/java/com/projectlyrics/server/global/slack/SlackClient.java
+++ src/main/java/com/projectlyrics/server/global/slack/SlackClient.java
@@ -8,6 +8,7 @@ import com.slack.api.methods.SlackApiException;
 import com.slack.api.methods.response.chat.ChatPostMessageResponse;
 import com.slack.api.model.block.Blocks;
 import com.slack.api.model.block.LayoutBlock;
+import com.slack.api.model.block.composition.MarkdownTextObject;
 import com.slack.api.model.block.composition.PlainTextObject;
 import com.slack.api.model.block.element.ButtonElement;
 import java.io.IOException;
@@ -52,16 +53,17 @@ public class SlackClient {
     public void sendNoteReportMessage(Report report) {
         // Build the blocks with buttons
         List<LayoutBlock> blocks = List.of(
-                Blocks.section(section -> section.text(PlainTextObject.builder().text(":rotating_light:*새로운 신고가 접수되었습니다.*").build())),
+                Blocks.section(section -> section.text(
+                        MarkdownTextObject.builder().text(":rotating_light:*새로운 신고가 접수되었습니다.*").build())),
                 Blocks.section(section -> section.fields(List.of(
-                        PlainTextObject.builder().text("*신고자 ID:*\n" + report.getReporter().getId()).build(),
-                        PlainTextObject.builder().text("*피신고자 ID:*\n" + report.getNote().getPublisher().getId()).build(),
-                        PlainTextObject.builder().text("*노트 ID:*\n" + report.getNote().getId()).build(),
-                        PlainTextObject.builder().text("*신고 이유:*\n" + report.getReportReason().getDescription()).build(),
-                        PlainTextObject.builder().text("*신고자 이메일:*\n" + (report.getEmail() != null ? report.getEmail() : "")).build(),
-                        PlainTextObject.builder().text("*신고 일시:*\n" + report.getCreatedAt()).build()
+                        MarkdownTextObject.builder().text("*신고자 ID:*\n" + report.getReporter().getId()).build(),
+                        MarkdownTextObject.builder().text("*피신고자 ID:*\n" + report.getNote().getPublisher().getId()).build(),
+                        MarkdownTextObject.builder().text("*노트 ID:*\n" + report.getNote().getId()).build(),
+                        MarkdownTextObject.builder().text("*신고 이유:*\n" + report.getReportReason().getDescription()).build(),
+                        MarkdownTextObject.builder().text("*신고자 이메일:*\n" + (report.getEmail() != null ? report.getEmail() : "-")).build(),
+                        MarkdownTextObject.builder().text("*신고 일시:*\n" + report.getCreatedAt()).build()
                 ))),
-                Blocks.section(section -> section.text(PlainTextObject.builder().text("*노트 내용:*\n" + report.getNote().getContent()).build())),
+                Blocks.section(section -> section.text(MarkdownTextObject.builder().text("*노트 내용:*\n" + report.getNote().getContent()).build())),
                 Blocks.actions(actions -> actions.elements(List.of(
                         ButtonElement.builder()
                                 .text(PlainTextObject.builder().text("Accept").build())
@@ -86,16 +88,16 @@ public class SlackClient {
     public void sendCommentReportMessage(Report report) {
         // Build the blocks with buttons
         List<LayoutBlock> blocks = List.of(
-                Blocks.section(section -> section.text(PlainTextObject.builder().text(":rotating_light:*새로운 신고가 접수되었습니다.*").build())),
+                Blocks.section(section -> section.text(MarkdownTextObject.builder().text(":rotating_light:*새로운 신고가 접수되었습니다.*").build())),
                 Blocks.section(section -> section.fields(List.of(
-                        PlainTextObject.builder().text("*신고자 ID:*\n" + report.getReporter().getId()).build(),
-                        PlainTextObject.builder().text("*피신고자 ID:*\n" + report.getComment().getWriter().getId()).build(),
-                        PlainTextObject.builder().text("*댓글 ID:*\n" + report.getComment().getId()).build(),
-                        PlainTextObject.builder().text("*신고 이유:*\n" + report.getReportReason().getDescription()).build(),
-                        PlainTextObject.builder().text("*신고자 이메일:*\n" + (report.getEmail() != null ? report.getEmail() : "")).build(),
-                        PlainTextObject.builder().text("*신고 일시:*\n" + report.getCreatedAt()).build()
+                        MarkdownTextObject.builder().text("*신고자 ID:*\n" + report.getReporter().getId()).build(),
+                        MarkdownTextObject.builder().text("*피신고자 ID:*\n" + report.getComment().getWriter().getId()).build(),
+                        MarkdownTextObject.builder().text("*댓글 ID:*\n" + report.getComment().getId()).build(),
+                        MarkdownTextObject.builder().text("*신고 이유:*\n" + report.getReportReason().getDescription()).build(),
+                        MarkdownTextObject.builder().text("*신고자 이메일:*\n" + (report.getEmail() != null ? report.getEmail() : "-")).build(),
+                        MarkdownTextObject.builder().text("*신고 일시:*\n" + report.getCreatedAt()).build()
                 ))),
-                Blocks.section(section -> section.text(PlainTextObject.builder().text("*댓글 내용:*\n" + report.getComment().getContent()).build())),
+                Blocks.section(section -> section.text(MarkdownTextObject.builder().text("*댓글 내용:*\n" + report.getComment().getContent()).build())),
                 Blocks.actions(actions -> actions.elements(List.of(
                         ButtonElement.builder()
                                 .text(PlainTextObject.builder().text("Accept").build())
