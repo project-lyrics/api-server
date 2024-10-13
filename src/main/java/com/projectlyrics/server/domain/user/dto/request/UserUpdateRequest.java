a6c9e3df39a0718de9868c8895577ea8f78f5405
Jin Geonwoo <jinkonu99@gmail.com>
2024-10-13 16:39:33 +0900

    [hotfix] refactor: 사용자 수정사항에 성별, 출생연도 추가

--- src/main/java/com/projectlyrics/server/domain/user/dto/request/UserUpdateRequest.java
+++ src/main/java/com/projectlyrics/server/domain/user/dto/request/UserUpdateRequest.java
@@ -1,9 +1,12 @@
 package com.projectlyrics.server.domain.user.dto.request;
 
+import com.projectlyrics.server.domain.user.entity.Gender;
 import com.projectlyrics.server.domain.user.entity.ProfileCharacter;
 
 public record UserUpdateRequest(
         String nickname,
-        ProfileCharacter profileCharacter
+        ProfileCharacter profileCharacter,
+        Gender gender,
+        Integer birthYear
 ) {
 }
