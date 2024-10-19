89f393376d0a48c72b18b6e036e23a2f1b2f2048
Jin Geonwoo <jinkonu99@gmail.com>
2024-10-19 21:02:45 +0900

    [hotfix] fix: FavoriteArtist의 참조 테이블 명시

--- src/main/java/com/projectlyrics/server/domain/favoriteartist/entity/FavoriteArtist.java
+++ src/main/java/com/projectlyrics/server/domain/favoriteartist/entity/FavoriteArtist.java
@@ -3,14 +3,7 @@ package com.projectlyrics.server.domain.favoriteartist.entity;
 import com.projectlyrics.server.domain.artist.entity.Artist;
 import com.projectlyrics.server.domain.common.entity.BaseEntity;
 import com.projectlyrics.server.domain.user.entity.User;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import jakarta.persistence.UniqueConstraint;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
@@ -20,6 +13,7 @@ import static com.projectlyrics.server.domain.common.util.DomainUtils.checkNull;
 
 @Getter
 @Entity
+@Table(name = "favorite_artists")
 @EqualsAndHashCode(of = "id", callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class FavoriteArtist extends BaseEntity {
