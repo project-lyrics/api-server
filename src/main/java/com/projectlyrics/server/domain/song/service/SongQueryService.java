7ba3391fd5e5cb7817ab73b42930bd368b3de6a3
Jin Geonwoo <jinkonu99@gmail.com>
2024-10-24 17:52:45 +0900

    [hotfix] chore: 커서 로깅

--- src/main/java/com/projectlyrics/server/domain/song/service/SongQueryService.java
+++ src/main/java/com/projectlyrics/server/domain/song/service/SongQueryService.java
@@ -7,10 +7,12 @@ import com.projectlyrics.server.domain.song.dto.response.SongSearchResponse;
 import com.projectlyrics.server.domain.song.exception.SongNotFoundException;
 import com.projectlyrics.server.domain.song.repository.SongQueryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
@@ -25,6 +27,8 @@ public class SongQueryService {
     }
 
     public CursorBasePaginatedResponse<SongGetResponse> searchSongsByArtist(Long artistId, String query, Long cursor, int size) {
+        log.info("cursor = {}", cursor);
+
         return CursorBasePaginatedResponse.of(
                 songQueryRepository.findAllByQueryAndArtistId(artistId, query, cursor, PageRequest.of(0, size))
                         .map(SongGetResponse::from)
