37f307a9f49b489875039de25a79ab1a24d96e45
elive7 <parksub020704@gmail.com>
2024-11-18 21:21:18 +0900

    [hotfix] fix: version 비교 로직 변경

--- src/main/java/com/projectlyrics/server/domain/auth/authentication/interceptor/VersionVerificationInterceptor.java
+++ src/main/java/com/projectlyrics/server/domain/auth/authentication/interceptor/VersionVerificationInterceptor.java
@@ -24,7 +24,7 @@ public class VersionVerificationInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         try {
-            if (!request.getHeader("App-Version").equals(version)) {
+            if (compareVersions(request.getHeader("App-Version"), version) < 0) {
                 throw new UpdateRequiredException();
             }
         } catch (NullPointerException e) {
@@ -32,5 +32,24 @@ public class VersionVerificationInterceptor implements HandlerInterceptor {
         }
 
         return true;
+    }
+
+    private static int compareVersions(String version1, String version2) {
+        String[] v1Parts = version1.split("\\.");
+        String[] v2Parts = version2.split("\\.");
+
+        int length = Math.max(v1Parts.length, v2Parts.length);
+
+        for (int i = 0; i < length; i++) {
+            int v1Part = i < v1Parts.length ? Integer.parseInt(v1Parts[i]) : 0;
+            int v2Part = i < v2Parts.length ? Integer.parseInt(v2Parts[i]) : 0;
+
+            if (v1Part < v2Part) {
+                return -1;
+            } else if (v1Part > v2Part) {
+                return 1;
+            }
+        }
+        return 0;
     }
 }
