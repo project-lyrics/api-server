ee988dde5071802301706afbb01447ef91532b62
elive7 <parksub020704@gmail.com>
2024-11-24 00:10:32 +0900

    [SCRUM-191] feat: 회원 (강제) 탈퇴시 차단 정보 삭제

--- src/main/java/com/projectlyrics/server/domain/block/repository/BlockCommandRepository.java
+++ src/main/java/com/projectlyrics/server/domain/block/repository/BlockCommandRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface BlockCommandRepository extends JpaRepository<Block, Long> {
 
+    void deleteAllByBlockerId(Long userId);
+    void deleteAllByBlockedId(Long userId);
 }
