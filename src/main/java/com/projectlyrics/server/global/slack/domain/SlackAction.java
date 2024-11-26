7e8eda48a5edc5eddda62ac64ab20d541739a46c
jin.geonwoo <jin.geonwoo@gmail.com>
2024-11-26 23:59:37 +0900

    [SCRUM-193] refactor: SlackController 리팩토링
    - Slack json을 파싱하는 부분을 SlackController에서 Slack 객체의 생성자로
    - json에서 직접 반복해서 꺼내는 것이 안이라 Slack에 getter를 호출하도록

---
+++ src/main/java/com/projectlyrics/server/global/slack/domain/SlackAction.java
@@ -0,0 +1,27 @@
+package com.projectlyrics.server.global.slack.domain;
+
+public enum SlackAction {
+
+    REPORT_ACCEPT,
+    REPORT_FAKE,
+    DISCIPLINE,
+    ;
+
+    public static SlackAction from(String actionId) {
+        if (actionId.startsWith("discipline")) {
+            return DISCIPLINE;
+        }
+
+        if (actionId.startsWith("report")) {
+            if (actionId.contains("accept")) {
+                return REPORT_ACCEPT;
+            }
+
+            if (actionId.contains("fake")) {
+                return REPORT_FAKE;
+            }
+        }
+
+        return null;
+    }
+}
