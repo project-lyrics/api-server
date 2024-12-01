eac2b43d2148a699db31048af09779f258788967
jin.geonwoo <jin.geonwoo@gmail.com>
2024-12-01 21:05:28 +0900

    [SCRUM-194] fix: 액세스 토큰 동적으로 변경하도록 수정
    - 이전에는 final 변수로 액세스 토큰을 설정함으로써, 각 API 요청마다 토큰을 갈아끼지 못하고 있었습니다.

--- src/main/java/com/projectlyrics/server/global/dev/cron/HttpRequestClient.java
+++ src/main/java/com/projectlyrics/server/global/dev/cron/HttpRequestClient.java
@@ -8,12 +8,11 @@ import java.net.http.HttpResponse;
 public class HttpRequestClient {
 
     private static final HttpClient client = HttpClient.newHttpClient();
-    private static final String accessToken = AccessTokenProvider.get();
 
     public static HttpResponse<String> send(String url) throws Exception {
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(url))
-                .header("Authorization", "Bearer " + accessToken)
+                .header("Authorization", "Bearer " + AccessTokenProvider.get())
                 .build();
 
         return client.send(request, HttpResponse.BodyHandlers.ofString());
