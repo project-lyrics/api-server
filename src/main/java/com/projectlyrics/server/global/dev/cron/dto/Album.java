359dcb09482e94556c3ced90c5411ededa329ec0
jin.geonwoo <jin.geonwoo@gmail.com>
2024-12-01 19:30:38 +0900

    [SCRUM-194] refactor: Album, Track에 Lombok getter 추가

--- src/main/java/com/projectlyrics/server/global/dev/cron/dto/Album.java
+++ src/main/java/com/projectlyrics/server/global/dev/cron/dto/Album.java
@@ -1,9 +1,14 @@
 package com.projectlyrics.server.global.dev.cron.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
+import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
+@Setter
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Album {
 
@@ -20,43 +25,7 @@ public class Album {
         this.id = id;
     }
 
-    public int getTotal_tracks() {
-        return total_tracks;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public List<Image> getImages() {
-        return images;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getRelease_date() {
-        return release_date;
-    }
-
-    public void setTotal_tracks(int total_tracks) {
-        this.total_tracks = total_tracks;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public void setImages(List<Image> images) {
-        this.images = images;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setRelease_date(String release_date) {
-        this.release_date = release_date;
+    public LocalDate getDate() {
+        return LocalDate.parse(release_date);
     }
 }
--- src/main/java/com/projectlyrics/server/global/dev/cron/dto/Track.java
+++ src/main/java/com/projectlyrics/server/global/dev/cron/dto/Track.java
@@ -1,9 +1,11 @@
 package com.projectlyrics.server.global.dev.cron.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Getter;
 
 import java.util.List;
 
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Track {
 
@@ -11,20 +13,4 @@ public class Track {
     private String name;
     private int track_number;
     private List<ArtistOfTrack> artists;
-
-    public String getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public int getTrack_number() {
-        return track_number;
-    }
-
-    public List<ArtistOfTrack> getArtists() {
-        return artists;
-    }
 }
