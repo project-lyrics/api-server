86e2078493eb35fc03477bddfde9c0f4e7b127c4
jin.geonwoo <jin.geonwoo@gmail.com>
2024-12-23 22:48:18 +0900

    [SCRUM-195] refactor: SongSearch 수정
    - OpenSearch에서 분석하도록 설정한 6개 프로퍼티에 대해 한국어인 경우와 아닐 경우 나눠서 저장하도록 수정했습니다.

--- src/main/java/com/projectlyrics/server/domain/search/domain/SongSearch.java
+++ src/main/java/com/projectlyrics/server/domain/search/domain/SongSearch.java
@@ -6,8 +6,12 @@ import java.util.List;
 
 public record SongSearch(
         Long id,
-        String title,
-        String artistName
+        String korean_title,
+        String english_title,
+        String korean_artist_name,
+        String english_artist_name,
+        String korean_album_name,
+        String english_album_name
 ) {
     public static final String INDEX = "song";
     public static final List<String> FIELDS = List.of(
@@ -22,8 +26,17 @@ public record SongSearch(
     public static SongSearch from(Song song) {
         return new SongSearch(
                 song.getId(),
-                song.getName(),
-                song.getArtist().getName()
+                isKorean(song.getName()) ? song.getName() : null,
+                isKorean(song.getName()) ? null : song.getName(),
+                isKorean(song.getArtist().getName()) ? song.getArtist().getName() : null,
+                isKorean(song.getArtist().getName()) ? null : song.getArtist().getName(),
+                isKorean(song.getAlbumName()) ? song.getAlbumName() : null,
+                isKorean(song.getAlbumName()) ? null : song.getAlbumName()
         );
+    }
+
+    private static boolean isKorean(String input) {
+        String koreanRegex = "^[\\uAC00-\\uD7A3]+$";
+        return input.matches(koreanRegex);
     }
 }
