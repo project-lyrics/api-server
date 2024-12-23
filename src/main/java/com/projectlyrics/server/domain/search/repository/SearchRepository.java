436d8ceffc12df1d11e4fb4e9f4046e8b2a99c5b
jin.geonwoo <jin.geonwoo@gmail.com>
2024-12-23 23:16:36 +0900

    [SCRUM-195] feat: SearchRepository.bulkIndex() 정의
    - 곡을 대량으로 한꺼번에 저장하기 위해 구현했습니다.

--- src/main/java/com/projectlyrics/server/domain/search/repository/SearchRepository.java
+++ src/main/java/com/projectlyrics/server/domain/search/repository/SearchRepository.java
@@ -7,15 +7,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.opensearch.client.opensearch.OpenSearchClient;
 import org.opensearch.client.opensearch._types.Refresh;
-import org.opensearch.client.opensearch.core.IndexRequest;
-import org.opensearch.client.opensearch.core.SearchRequest;
-import org.opensearch.client.opensearch.core.SearchResponse;
+import org.opensearch.client.opensearch.core.*;
+import org.opensearch.client.opensearch.core.bulk.BulkOperation;
+import org.opensearch.client.opensearch.core.bulk.CreateOperation;
 import org.opensearch.client.opensearch.core.search.Hit;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Objects;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -24,7 +24,6 @@ public class SearchRepository {
 
     private final OpenSearchClient searchClient;
 
-    @Async
     public void index(SongSearch songSearch) {
         try {
             searchClient.index(IndexRequest.of(builder ->
@@ -36,6 +35,37 @@ public class SearchRepository {
             ));
         } catch (IOException e) {
             log.error("error occurred indexing a document.");
+            throw new FailedToIndexException();
+        }
+    }
+
+    public void bulkIndex(List<SongSearch> songSearchList) {
+        try {
+            BulkRequest.Builder bulkRequestBuilder = new BulkRequest.Builder();
+
+            songSearchList.forEach(search -> {
+                BulkOperation operation = new BulkOperation.Builder()
+                        .create(new CreateOperation.Builder<>()
+                                .index(SongSearch.INDEX)
+                                .id(search.id().toString())
+                                .document(search)
+                                .build())
+                        .build();
+
+                bulkRequestBuilder.operations(operation);
+            });
+
+            BulkResponse response = searchClient.bulk(bulkRequestBuilder.build());
+
+            if (response.errors()) {
+                response.items().forEach(item -> {
+                    if (Objects.nonNull(item.error())) {
+                        log.error(item.error().reason());
+                    }
+                });
+            }
+        } catch (IOException e) {
+            log.error("error occurred bulk-indexing a document.");
             throw new FailedToIndexException();
         }
     }
