2c29498f377a05437b87153a61c3dd35b255e225
elive7 <parksub020704@gmail.com>
2025-01-28 14:55:53 +0900

    [hotfix] feat: UpdateRequiredException에 data값 추가하도록 수정
    - 앱 업데이트 안내시, 앱스토어로 이동할 수 있도록 url이라는 data 필요
    - 이를 위해 모든 exception에 data 필드 추가

--- src/main/java/com/projectlyrics/server/global/exception/FeelinException.java
+++ src/main/java/com/projectlyrics/server/global/exception/FeelinException.java
@@ -8,24 +8,47 @@ import com.projectlyrics.server.domain.common.message.ErrorCode;
 public class FeelinException extends RuntimeException {
 
     private final ErrorCode errorCode;
+    private final Object data;
 
     public FeelinException(ErrorCode errorCode) {
         super(errorCode.getErrorMessage());
         this.errorCode = errorCode;
+        this.data = null;
     }
 
     public FeelinException(ErrorCode errorCode, String messageForLog) {
         super(messageForLog);
         this.errorCode = errorCode;
+        this.data = null;
     }
 
     public FeelinException(ErrorCode errorCode, Exception originalException) {
         super(originalException);
         this.errorCode = errorCode;
+        this.data = null;
     }
 
     public FeelinException(ErrorCode errorCode, String messageForLog, Exception originalException) {
         super(messageForLog, originalException);
         this.errorCode = errorCode;
+        this.data = null;
+    }
+
+    public FeelinException(ErrorCode errorCode, Object data) {
+        super(errorCode.getErrorMessage());
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public FeelinException(ErrorCode errorCode, String messageForLog, Object data) {
+        super(messageForLog);
+        this.errorCode = errorCode;
+        this.data = data;
+    }
+
+    public FeelinException(ErrorCode errorCode, String messageForLog, Exception originalException, Object data) {
+        super(messageForLog, originalException);
+        this.errorCode = errorCode;
+        this.data = data;
     }
 }
--- src/main/java/com/projectlyrics/server/global/exception/UpdateRequiredException.java
+++ src/main/java/com/projectlyrics/server/global/exception/UpdateRequiredException.java
@@ -1,10 +1,11 @@
 package com.projectlyrics.server.global.exception;
 
 import com.projectlyrics.server.domain.common.message.ErrorCode;
+import java.util.Map;
 
 public class UpdateRequiredException extends FeelinException {
 
     public UpdateRequiredException() {
-        super(ErrorCode.UPDATE_REQUIRED);
+        super(ErrorCode.UPDATE_REQUIRED, Map.of("appStoreUrl", "https://apps.apple.com/kr/app/6738319829"));
     }
 }
