69396fce0b5c19f116ead17737c8ba488c1e48a8
jin.geonwoo <jin.geonwoo@gmail.com>
2025-02-11 22:16:48 +0900

    [SCRUM-198] test: EventCommandServiceTest.이벤트를_거부할_수_있다() 작성

--- src/test/java/com/projectlyrics/server/domain/event/service/EventCommandServiceTest.java
+++ src/test/java/com/projectlyrics/server/domain/event/service/EventCommandServiceTest.java
@@ -1,9 +1,14 @@
 package com.projectlyrics.server.domain.event.service;
 
 import com.projectlyrics.server.domain.event.domain.Event;
+import com.projectlyrics.server.domain.event.domain.EventReceipt;
 import com.projectlyrics.server.domain.event.dto.request.EventCreateRequest;
-import com.projectlyrics.server.domain.event.repository.EventCommandRepository;
+import com.projectlyrics.server.domain.event.repository.EventQueryRepository;
+import com.projectlyrics.server.domain.event.repository.EventReceiptQueryRepository;
+import com.projectlyrics.server.domain.user.entity.User;
+import com.projectlyrics.server.domain.user.repository.UserCommandRepository;
 import com.projectlyrics.server.support.IntegrationTest;
+import com.projectlyrics.server.support.fixture.UserFixture;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -15,7 +20,13 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 class EventCommandServiceTest extends IntegrationTest {
 
     @Autowired
-    EventCommandRepository eventCommandRepository;
+    UserCommandRepository userCommandRepository;
+
+    @Autowired
+    EventQueryRepository eventQueryRepository;
+
+    @Autowired
+    EventReceiptQueryRepository eventReceiptQueryRepository;
 
     @Autowired
     EventCommandService sut;
@@ -26,13 +37,33 @@ class EventCommandServiceTest extends IntegrationTest {
         EventCreateRequest request = new EventCreateRequest("imageUrl", "redirectUrl", LocalDate.now());
 
         // when
-        Event result = sut.create(request);
+        Event event = sut.create(request);
 
         // then
+        Event result = eventQueryRepository.findById(event.getId());
         assertAll(
                 () -> assertThat(result.getImageUrl()).isEqualTo(request.imageUrl()),
                 () -> assertThat(result.getRedirectUrl()).isEqualTo(request.redirectUrl()),
-                () -> assertThat(result.getDueDate()).isEqualTo(request.dueDate())
+                () -> assertThat(result.getDueDate()).isEqualTo(request.dueDate().atStartOfDay())
+        );
+    }
+
+    @Test
+    void 이벤트를_거부할_수_있다() {
+        // given
+        User user = userCommandRepository.save(UserFixture.create());
+
+        EventCreateRequest request = new EventCreateRequest("imageUrl", "redirectUrl", LocalDate.now());
+        Event event = sut.create(request);
+
+        // when
+        sut.refuse(event.getId(), user.getId());
+
+        // then
+        EventReceipt result = eventReceiptQueryRepository.findByEventIdAndUserId(event.getId(), user.getId());
+        assertAll(
+                () -> assertThat(result.getEvent().getId()).isEqualTo(event.getId()),
+                () -> assertThat(result.getUser().getId()).isEqualTo(user.getId())
         );
     }
 }
