09835243fae9485a0f53cca1144fc7d590e3e19f
elive7 <parksub020704@gmail.com>
2025-02-12 23:04:34 +0900

    [SCRUM-198] test: 테스트를 위한 EventFixture 정의
    - 테스트를 위해서 EventFixture를 정의하였고, 그에 맞게 Event 수정

--- src/main/java/com/projectlyrics/server/domain/event/domain/Event.java
+++ src/main/java/com/projectlyrics/server/domain/event/domain/Event.java
@@ -1,19 +1,24 @@
 package com.projectlyrics.server.domain.event.domain;
 
 import com.projectlyrics.server.domain.common.entity.BaseEntity;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Entity
 @Table(name = "events")
 @EqualsAndHashCode(of = "id", callSuper = false)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Event extends BaseEntity {
 
     @Id
@@ -36,6 +41,15 @@ public class Event extends BaseEntity {
 
     public static Event create(EventCreate eventCreate) {
         return new Event(
+                eventCreate.imageUrl(),
+                eventCreate.redirectUrl(),
+                eventCreate.dueDate()
+        );
+    }
+
+    public static Event createWithId(Long id, EventCreate eventCreate) {
+        return new Event(
+                id,
                 eventCreate.imageUrl(),
                 eventCreate.redirectUrl(),
                 eventCreate.dueDate()
