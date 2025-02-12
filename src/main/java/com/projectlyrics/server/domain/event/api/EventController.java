483c38f93053d5358b179436073cd72994b8cf15
elive7 <parksub020704@gmail.com>
2025-02-12 22:05:11 +0900

    [SCRUM-198] feat: EventController 내 이벤트 조회 api 작성

--- src/main/java/com/projectlyrics/server/domain/event/api/EventController.java
+++ src/main/java/com/projectlyrics/server/domain/event/api/EventController.java
@@ -2,13 +2,22 @@ package com.projectlyrics.server.domain.event.api;
 
 import com.projectlyrics.server.domain.auth.authentication.AuthContext;
 import com.projectlyrics.server.domain.auth.authentication.Authenticated;
+import com.projectlyrics.server.domain.common.dto.util.CursorBasePaginatedResponse;
 import com.projectlyrics.server.domain.event.dto.request.EventCreateRequest;
 import com.projectlyrics.server.domain.event.dto.response.EventCreateResponse;
+import com.projectlyrics.server.domain.event.dto.response.EventGetResponse;
 import com.projectlyrics.server.domain.event.dto.response.EventRefusalResponse;
 import com.projectlyrics.server.domain.event.service.EventCommandService;
+import com.projectlyrics.server.domain.event.service.EventQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/events")
@@ -16,6 +25,7 @@ import org.springframework.web.bind.annotation.*;
 public class EventController {
 
     private final EventCommandService eventCommandService;
+    private final EventQueryService eventQueryService;
 
     @PostMapping
     public ResponseEntity<EventCreateResponse> create(
@@ -25,6 +35,19 @@ public class EventController {
 
         return ResponseEntity
                 .ok(new EventCreateResponse(true));
+    }
+
+    @GetMapping
+    public ResponseEntity<CursorBasePaginatedResponse<EventGetResponse>> getAllExcludingRefusals(
+            @Authenticated AuthContext authContext,
+            @RequestParam(name = "cursor", required = false) Long cursor,
+            @RequestParam(name = "size", defaultValue = "10") int size
+    ) {
+        CursorBasePaginatedResponse<EventGetResponse> response = eventQueryService.getAllExcludingRefusals(authContext.getId(), cursor, size);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(response);
     }
 
     @PostMapping("/refuse")
