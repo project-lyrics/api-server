925cf110f87c1dc31812806da1310d76c87c7cc6
elive7 <parksub020704@gmail.com>
2025-03-11 23:49:02 +0900

    [SCRUM-198] feat: 이벤트 거절 유효 기간 유동적으로 설정할 수 있도록 변경
    - application.yml에 거절 유효 기간을 넣고, 그 값을 이용해서 거절을 유동적으로 할 수 있도록 수정
    - EventRefusal에 deadline이라는 필드를 추가하여, 해당 거절이 언제까지 유효한지 기록
    - 이벤트 목록 조회시, 클라에서 거절 버튼 문구를 변경할 수 있도록 refusalPeriod 함께 제공

--- src/main/java/com/projectlyrics/server/domain/event/api/EventController.java
+++ src/main/java/com/projectlyrics/server/domain/event/api/EventController.java
@@ -5,20 +5,30 @@ import com.projectlyrics.server.domain.auth.authentication.Authenticated;
 import com.projectlyrics.server.domain.common.dto.util.CursorBasePaginatedResponse;
 import com.projectlyrics.server.domain.event.dto.request.EventCreateRequest;
 import com.projectlyrics.server.domain.event.dto.response.EventCreateResponse;
+import com.projectlyrics.server.domain.event.dto.response.EventCursorBasePaginatedResponse;
 import com.projectlyrics.server.domain.event.dto.response.EventGetResponse;
 import com.projectlyrics.server.domain.event.dto.response.EventRefusalResponse;
 import com.projectlyrics.server.domain.event.service.EventCommandService;
 import com.projectlyrics.server.domain.event.service.EventQueryService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
 public class EventController {
 
+    @Value("${event.refusal_period:1}")
+    private int refusalPeriod;
     private final EventCommandService eventCommandService;
     private final EventQueryService eventQueryService;
 
@@ -33,7 +43,7 @@ public class EventController {
     }
 
     @GetMapping
-    public ResponseEntity<CursorBasePaginatedResponse<EventGetResponse>> getAllExceptRefused(
+    public ResponseEntity<EventCursorBasePaginatedResponse> getAllExceptRefused(
             @Authenticated AuthContext authContext,
             @RequestHeader("Device-Id") String deviceId,
             @RequestParam(name = "cursor", required = false) Long cursor,
@@ -49,7 +59,7 @@ public class EventController {
 
         return ResponseEntity
                 .status(HttpStatus.OK)
-                .body(response);
+                .body(new EventCursorBasePaginatedResponse(refusalPeriod, response));
     }
 
     @PostMapping("/refuse")
@@ -59,9 +69,9 @@ public class EventController {
             @RequestParam("eventId") Long eventId
     ) {
         if (authContext.isAnonymous()) {
-            eventCommandService.refuseByDeviceId(eventId, deviceId);
+            eventCommandService.refuseByDeviceId(eventId, deviceId, refusalPeriod);
         } else {
-            eventCommandService.refuseByUser(eventId, authContext.getId());
+            eventCommandService.refuseByUser(eventId, authContext.getId(), refusalPeriod);
         }
 
         return ResponseEntity
