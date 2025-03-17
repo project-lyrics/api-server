a6eca145af351a15d41f13ac3a3cdf14cc31d9ab
elive7 <parksub020704@gmail.com>
2025-03-18 00:22:23 +0900

    [SCRUM-199] feat: 배너 도메인 정의
    - 배너 도메인을 이벤트 도메인과 거의 비슷한 형태로 정의했습니다.

---
+++ src/main/java/com/projectlyrics/server/domain/banner/dto/request/BannerCreateRequest.java
@@ -0,0 +1,12 @@
+package com.projectlyrics.server.domain.banner.dto.request;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import java.time.LocalDate;
+
+public record BannerCreateRequest(
+        String imageUrl,
+        String redirectUrl,
+        @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd", timezone = "Asia/Seoul")
+        LocalDate dueDate
+) {
+}
