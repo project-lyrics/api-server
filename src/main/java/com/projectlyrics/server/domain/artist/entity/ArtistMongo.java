b5a441e7aff7b23c59ad853d837077f92c4252da
elive7 <parksub020704@gmail.com>
2025-08-16 00:47:23 +0900

    [SCRUM-213] feat: mongodb에서 사용할 ArtistMongo 도메인 작성

---
+++ src/main/java/com/projectlyrics/server/domain/artist/entity/ArtistMongo.java
@@ -0,0 +1,33 @@
+package com.projectlyrics.server.domain.artist.entity;
+
+import jakarta.persistence.Id;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+@Document(collection = "artists")
+public class ArtistMongo {
+    @Id
+    private final Long id;
+
+    @Field("search_names")
+    private final String searchNames;
+
+    private ArtistMongo(Long id, String searchNames) {
+        this.id = id;
+        this.searchNames = searchNames;
+    }
+
+    public static ArtistMongo of(Artist artist) {
+        String searchNames = Stream.of(
+                        artist.getName(),
+                        artist.getSecondName(),
+                        artist.getThirdName())
+                .filter(Objects::nonNull)
+                .collect(Collectors.joining(" "));
+        return new ArtistMongo(artist.getId(), searchNames);
+    }
+}
+
